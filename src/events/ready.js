c4eb71d9019bbbf4b084bf1ff2ca20d2411fd7e8
Dumpster <84399187+Dumpsterpog@users.noreply.github.com>
2022-02-16 14:49:36 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,7 @@
 module.exports = async (client) => {
     console.log("Client is up");
 // 936942255451803648
-    client.application.commands.set([...client.commands.map(v => v.data), "936942255451803648"], "936942255451803648");
+    client.application.commands.set([...client.commands.map(v => v.data)], "936942255451803648");
 
     
 }
